9a0307d6d2b11ee8bd0fb86b484f92d20bc3bb86
isamuu <64524618+isamuu@users.noreply.github.com>
2023-09-12 12:07:57 +0200

    Update dashboard_zec_github.py

--- dashboard_zec_github.py
+++ dashboard_zec_github.py
@@ -210,7 +210,7 @@ def homepage():
          cols = st.columns(4)
          
          icon_bakwagen = "https://raw.githubusercontent.com/isamuu/dashboard/main/Icons%20dashboard/db%20bakwagen.png"
-         aantal_bakwagen = df[df["Datum"]=="2022-01-01 00:00:00"]["aantal bakwagen"].sum()
+         aantal_bakwagen = round(df[df["Datum"]=="2022-01-01 00:00:00"]["aantal bakwagen"].sum(), 0)
          icon_bakwagen_html = f'''<img src="{icon_bakwagen}" width="150" style="display: block; margin: auto;">'
          <p style="text-align: center;">{aantal_bakwagen} Bakwagens</p>'''
          
