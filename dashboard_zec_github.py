4121614a32591386569e768fd94003881742fa66
isamuu <64524618+isamuu@users.noreply.github.com>
2023-09-18 10:15:14 +0200

    Update dashboard_zec_github.py

--- dashboard_zec_github.py
+++ dashboard_zec_github.py
@@ -549,7 +549,15 @@ def vehicle_page():
          col1, col2 = st.columns(2)
 
          # Place text in the left column
-         col1.text("Your Text Here")  # Replace with your desired text
+         col1.text(""" Dit dashboard geeft inzicht over het gebruik van elektriciteit op het bedrijventerrein Schiphol Trade Park. 
+         Met de transitie naar elektrische voertuigen zal er meer gevraagd worden van het netwerk. 
+         Door de ontwikkeling van de bedrijven en hun wagenpark in kaart te brengen kan er voorspeld worden hoe de stroomvraag zich ontwikkeld.
+         
+         \n\n Terwijl de wereld zich richt op duurzame energie, wordt de overstap naar elektrische voertuigen op bedrijventerreinen een groot en complex vraagstuk. 
+         Het is algemeen bekend dat de elektriciteitsinfrastructuur in Nederland op sommige locaties al tegen zijn uiterste loopt.
+         De transitie naar eleketrisch transport zal nog meer druk zetten op onze infrastructuur, zo ook op het bedrijventerrein Schiphol Trade Park.
+         Dit betekent dat we geconfronteerd worden met een complexe puzzel die op de juiste manier moet worden aangepakt. 
+         Dit dashboard biedt inzichten in de huidige situatie, uitdagingen en kansen binnen op het Schiphol Trade Park.""")  # Replace with your desired text
 
          # capacity line
          show_line = col1.checkbox('Capaciteit netwerk')
