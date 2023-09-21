b2967fc910e8e439d7b346d3f89a744453361265
isamuu <64524618+isamuu@users.noreply.github.com>
2023-09-21 22:06:43 +0200

    Update dashboard_zec_github.py

--- dashboard_zec_github.py
+++ dashboard_zec_github.py
@@ -677,7 +677,7 @@ def vehicle_page():
 
          adjustment_value = col1.number_input('upgrade netwerk', value=0.0)
 
-         sndr_park
+         df = pd.read_excel("Data/Data Sander.xlsx")
 
          df
 
