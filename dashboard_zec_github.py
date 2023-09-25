87a79d2d21e1a44545df65d8146e593d95922e1f
isamuu <64524618+isamuu@users.noreply.github.com>
2023-09-25 17:58:53 +0200

    Update dashboard_zec_github.py

--- dashboard_zec_github.py
+++ dashboard_zec_github.py
@@ -808,11 +808,7 @@ def vehicle_page():
          toename[['type', 'jaar']] = toename['index'].str.split('  ', expand=True)
          toename['jaar'] = pd.to_numeric(toename['jaar'])
          toename = toename[['jaar', 'type', 'max', 'min', 'gem']]
-         
-       
-         toename[['type', 'jaar']] = toename['Date'].str.split('  ', expand=True)
-         toename['jaar'] = pd.to_numeric(toename['jaar'])
-         toename = toename[['jaar', 'type', 'max', 'min', 'gem']]
+
          
 
          
