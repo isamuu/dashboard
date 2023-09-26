860c6b826554164392afaa2e24e7257736f10311
isamuu <64524618+isamuu@users.noreply.github.com>
2023-09-26 22:02:54 +0200

    Update dashboard_zec_github.py

--- dashboard_zec_github.py
+++ dashboard_zec_github.py
@@ -823,7 +823,7 @@ def vehicle_page():
          # Plotting
          fig7, ax7 = plt.subplots()
          ax7.plot(filtered_toename['jaar'], filtered_toename['min'])
-         ax7.fill_between(filtered_toename['jaar'], filtered_toename['min'], filtered_toename['max'])
+         ax7.fill_between(filtered_toename['jaar'], filtered_toename['min'], filtered_toename['max'], color = "skyblue", alpha = 0.4)
          plt.xlabel('Jaar')
          plt.ylabel('Value')
          plt.legend()
