c8c8a2be8e1aa4cc363165451d02028be568bd62
isamuu <64524618+isamuu@users.noreply.github.com>
2023-09-29 10:13:24 +0200

    Update dashboard_zec_github.py

--- dashboard_zec_github.py
+++ dashboard_zec_github.py
@@ -835,7 +835,7 @@ def vehicle_page():
          fig7, ax7 = plt.subplots()
          ax7.plot(filtered_toename['jaar'], filtered_toename['gem'])
          ax7.fill_between(filtered_toename['jaar'], filtered_toename['min'], filtered_toename['max'], color = "skyblue", alpha = 0.4)
-         ax7.ylim([0,8000])
+         ax7.set_ylim([0,8000])
          plt.xlabel('Jaar')
          plt.ylabel('Value')
          plt.legend()
@@ -881,7 +881,7 @@ def vehicle_page():
          # Adjusting axis label font sizes
          ax5.set_xlabel('Year', fontsize=6)
          ax5.set_ylabel('Value', fontsize=6)
-         ax5.ylim([0,8000])
+         ax5.set_ylim([0,8000])
          
          # Adjusting tick font sizes
          ax5.tick_params(axis='both', which='major', labelsize=6)
